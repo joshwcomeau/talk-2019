5f9ac75831a5bb8ccf35c907546c84128ef67c65
Joshua Comeau <joshwcomeau@gmail.com>
2019-03-10 10:54:21 -0400

    Set up project

---
+++ src/components/AnimatedFire/AnimatedFire.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+import fireSpritesheet from '../../../assets/fire-sprite-row.png'
+
+const AnimatedFire = ({ width = 38, height = 67 }) => {
+    return <Fire style={{ width, height }} />
+}
+
+const animation = keyframes`
+  100% {
+    background-position: -380px;
+  }
+`
+
+const Fire = styled.div`
+  position: relative;
+  background-image: url('${fireSpritesheet}');
+  background-size: cover;
+  animation: ${animation} 0.8s steps(10) infinite;
+  backface-visibility: hidden;
+`
+
+export default AnimatedFire
