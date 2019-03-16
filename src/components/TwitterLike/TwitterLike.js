6d887e6a30e23d2feb7d6b36426b423460be0b4b
Joshua Comeau <joshwcomeau@gmail.com>
2019-03-16 13:10:39 -0400

    WIP

--- src/components/TwitterLike/TwitterLike.js
+++ src/components/TwitterLike/TwitterLike.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import PoppingCircle from '../PoppingCircle'
+import UnstyledButton from '../UnstyledButton'
+
+import heartSrc from './heart.svg'
 
 const TwitterLike = () => {
   const [isToggled, setIsToggled] = React.useState(false)
   return (
-    <button onClick={() => setIsToggled(!isToggled)}>
-      <PoppingCircle key={isToggled} />
-    </button>
+    <Wrapper onClick={() => setIsToggled(!isToggled)}>
+      <Background>{isToggled && <PoppingCircle />}</Background>
+
+      <Heart src={heartSrc} />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled(UnstyledButton)`
+  position: relative;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Background = styled.div`
+  position: absolute;
+  z-index: -1;
+`
+
+const Heart = styled.img`
+  width: 20px;
+`
 
 export default TwitterLike
---
+++ src/components/UnstyledButton/UnstyledButton.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export default styled.button`
+  display: block;
+  margin: 0;
+  padding: 0;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  text-align: left;
+
+  &:focus {
+    outline: 2px solid hotpink;
+    outline-offset: 2px;
+  }
+
+  &:focus:not(.focus-visible) {
+    outline: none;
+  }
+`
