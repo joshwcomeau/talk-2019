559222533eb79e3471be0ec6c141509e89b56523
Joshua Comeau <joshwcomeau@gmail.com>
2019-03-24 09:29:15 -0400

    Add TitleCard component + assets + webpack config

---
+++ src/components/TitleCard/TitleCard.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import bokehBg from '../../../assets/flame.mp4'
+import { colors } from '../../../presentation/theme'
+
+const TitleCard = () => {
+  return (
+    <Wrapper>
+      <WordVideo autoPlay loop src={bokehBg} />
+
+      <Svg viewBox="0 0 1600 900" preserveAspectRatio="xMidYMid slice">
+        <defs>
+          <mask id="title-card-mask" x={0} y={0} width="100%" height="100%">
+            <rect x={0} y={0} width="100%" height="100%" fill="#FFF" />
+            <Line1 textAnchor="middle" x={800} y={300}>
+              SAVING THE WEB
+            </Line1>
+            <Line2 textAnchor="middle" x={800} y={500}>
+              16 MILLISECONDS
+            </Line2>
+            <Line3 textAnchor="middle" x={800} y={740}>
+              AT A TIME
+            </Line3>
+          </mask>
+        </defs>
+        <rect
+          mask="url(#title-card-mask)"
+          fill="#000"
+          x={0}
+          y={0}
+          width="100%"
+          height="100%"
+        />
+      </Svg>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #000;
+`
+
+const WordVideo = styled.video`
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`
+
+const Svg = styled.svg`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+`
+
+const Text = styled.text`
+  font-weight: 900;
+  letter-spacing: -5px;
+  font-family: 'Oswald', sans-serif;
+`
+
+const Line1 = styled(Text)`
+  font-size: 196px;
+`
+const Line2 = styled(Text)`
+  font-size: 196px;
+`
+const Line3 = styled(Text)`
+  font-size: 244px;
+`
+
+export default TitleCard
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,57 +1,57 @@
 /* eslint-disable */
 
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-  mode: "production",
-  entry: ["babel-polyfill", "./index"],
+  mode: 'production',
+  entry: ['babel-polyfill', './index'],
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist/"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/',
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    })
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
   ],
   optimization: {
-    minimize: true
+    minimize: true,
   },
   module: {
     rules: [
       {
         test: /\.md$/,
-        loader: "html-loader!markdown-loader?gfm=false"
+        loader: 'html-loader!markdown-loader?gfm=false',
       },
       {
         test: /\.mdx$/,
         exclude: /node_modules/,
         use: [
-          { loader: "babel-loader" },
-          { loader: require.resolve("./loader.js") }
-        ]
+          { loader: 'babel-loader' },
+          { loader: require.resolve('./loader.js') },
+        ],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
+        loader: 'style-loader!css-loader',
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        loader: "url-loader?limit=8192"
+        test: /\.(png|jpg|gif|mp4)$/,
+        loader: 'url-loader?limit=8192',
       },
       {
         test: /\.svg$/,
-        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-      }
-    ]
-  }
-};
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
+}
